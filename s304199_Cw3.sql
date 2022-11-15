d0ad8e26cb7a237ab1d57a98303c9a1b25332c56
marekszlapa <marekszlapa@o2.pl>
2022-11-15 14:15:15 +0100

    Cw3

---
+++ s304199_Cw3.sql
@@ -0,0 +1,13 @@
+CREATE PROCEDURE Cw3_304199
+
+@YearsAgo AS INT
+
+AS
+BEGIN
+SELECT * FROM AdventureWorksDW2019.dbo.FactCurrencyRate AS FCR INNER JOIN AdventureWorksDW2019.dbo.DimCurrency AS DC 
+ON DC.CurrencyKey=FCR.CurrencyKey WHERE DATEPART(YEAR, FCR.Date) < DATEPART(YEAR, GETDATE()) - @YearsAgo 
+AND DATEPART(MONTH, GETDATE()) < DATEPART(MONTH, FCR.Date) AND  DATEPART(DAY, GETDATE()) < DATEPART(DAY, FCR.Date)
+AND (DC.CurrencyAlternateKey = 'EUR' OR DC.CurrencyAlternateKey = 'GBP');
+END
+
+--EXECUTE Cw3_304199 @YearsAgo = 8 
